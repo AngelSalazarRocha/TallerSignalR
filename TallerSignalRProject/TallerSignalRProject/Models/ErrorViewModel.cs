18a3903e64b00d588f7404e396abc3e79941f673
Angel S. <angel.rochaa46281@outlook.com>
2024-09-21 19:36:58 -0500

    Initial local commit

---
+++ TallerSignalRProject/TallerSignalRProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TallerSignalRProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
