270dcec6115b6554490a7aff7da71f35a5d18257
Angel S. <angel.rochaa46281@outlook.com>
2024-09-23 23:36:58 -0500

    CLASE HUB, CONFIGURAR EN PROGRAM, USAR LA LIBRERIA

---
+++ TallerSignalRProject/TallerSignalRProject/Hubs/Messages/MessageHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TallerSignalRProject.Hubs.Messages
+{
+    public class MessageHub : Hub
+    {
+        public async void Send(string param1, string param2)
+        {
+            await Clients.All.SendAsync("Receive", param1, param2);
+        }
+    }
+}
--- TallerSignalRProject/TallerSignalRProject/Program.cs
+++ TallerSignalRProject/TallerSignalRProject/Program.cs
@@ -1,7 +1,12 @@
+using TallerSignalRProject.Hubs.Messages;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Adding signal R
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -23,5 +28,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<MessageHub>("/messageHub");
 
 app.Run();
